99599d61861f5ecb35d1412fc872e92ff6c411f2
nkitts1319 <nkitts1319@gmail.com>
2024-11-07 06:35:23 +0900

    アドバイスに沿ってコードの修正

--- php-practice/php-practice_2.php
+++ php-practice/php-practice_2.php
@@ -3,11 +3,11 @@
 echo "1から100までのカウントを開始します\n\n";
 
 for ($i = 1; $i <= 100; $i++) {
-    if ($i % 4 == 0 && $i % 5 == 0) {
+    if ($i % 4 === 0 && $i % 5 === 0) {
         echo "tic-tac\n";
-    } elseif ($i % 4 == 0) {
+    } elseif ($i % 4 === 0) {
         echo "tic\n";
-    } elseif ($i % 5 == 0) {
+    } elseif ($i % 5 === 0) {
         echo "tac\n";
     } else {
         echo "$i\n";
